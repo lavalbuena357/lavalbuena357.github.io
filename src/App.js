e01671fdf5a3d03d2427ff4e0f2037484aef9bf6
albertvr357 <cuervo357@gmail.com>
2021-07-16 12:13:15 -0500

    footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path='/' component={Header} />
       <Route path='/' component={Nav} />
       <Route exact path='/' component={Main} />
+      <Route path='/' component={Footer} />
     </div>
   );
 }
