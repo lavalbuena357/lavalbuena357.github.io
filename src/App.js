0dab89ed6b11f00203852d77c963fff15615c5cf
albertvr357 <cuervo357@gmail.com>
2021-07-16 12:15:56 -0500

    contact module

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path='/' component={Header} />
       <Route path='/' component={Nav} />
       <Route exact path='/' component={Main} />
+      <Route path='/contacto' component={Contact} />
       <Route path='/' component={Footer} />
     </div>
   );
