f5327a02acffe10c8068eb4e82f82808067ed862
albertvr357 <cuervo357@gmail.com>
2021-07-16 11:35:22 -0500

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Route path='/' component={Header} />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './Header.module.css';
+import logo from '../../utils/images/logo-av.svg';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+      <div className={styles.container}>
+          <div className={styles.img_container}>
+              <Link to='/'>
+                  <img src={logo} alt="logo albert valbuena" />
+                  <div className={styles.logo_text}>
+                      <span>Albert Valbuena</span>
+                      <span>Full Stack Developer</span>
+                  </div>
+              </Link>
+          </div>
+      </div>
+  )
+};
+
+export default Header;
