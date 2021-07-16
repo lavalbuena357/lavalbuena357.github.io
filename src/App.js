8fe9ac3218b2aa59782c3fa5de19167ec22e69a7
albertvr357 <cuervo357@gmail.com>
2021-07-16 11:49:34 -0500

    nav component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
       <Route path='/' component={Header} />
+      <Route path='/' component={Nav} />
     </div>
   );
 }
