4fcdeab0ae2fdd5b8de2c37ae9afd6ebec76ca43
albertvr357 <cuervo357@gmail.com>
2021-07-23 13:52:10 -0500

    skills data

---
+++ src/utils/skillsData.js
@@ -0,0 +1,142 @@
+import { 
+  SiJavascript, 
+  SiCss3, 
+  SiHtml5, 
+  SiReact, 
+  SiRedux,  
+  SiBootstrap,
+  SiNodeDotJs, 
+  SiPostgresql, 
+  SiMysql, 
+  SiWordpress, 
+  SiWoo, 
+  SiPrestashop, 
+  SiGit, 
+  SiGithub, 
+  SiAdobephotoshop,
+  SiAdobeillustrator,
+  SiAngularjs,
+  SiMaterialUi,
+  SiFirebase,
+  SiGraphql,
+  SiTypescript,
+  SiMongodb,
+  SiVueDotJs,
+  SiSass,
+  SiSocketDotIo } from "react-icons/si"; 
+
+export const skills = [
+  {
+    name: 'CSS 3',
+    icon: <SiCss3 />,
+  },
+  {
+    name: 'HTML 5',
+    icon: <SiHtml5 />,
+  },
+  {
+    name: 'JavaScript',
+    icon: <SiJavascript />,
+  },
+  {
+    name: 'React.Js',
+    icon: <SiReact />,
+  },
+  {
+    name: 'Redux',
+    icon: <SiRedux />,
+  },
+  {
+    name: 'Bootstrap',
+    icon: <SiBootstrap />,
+  },
+  {
+    name: 'Node.Js',
+    icon: <SiNodeDotJs />,
+  },
+  {
+    name: 'Express',
+    icon: 'Express',
+  },
+  {
+    name: 'PostgresSQL',
+    icon: <SiPostgresql />,
+  },
+  {
+    name: 'MySQL',
+    icon: <SiMysql />,
+  },
+  {
+    name: 'Wordpress',
+    icon: <SiWordpress />,
+  },
+  {
+    name: 'WooCommerce',
+    icon: <SiWoo />,
+  },
+  {
+    name: 'PrestaShop',
+    icon: <SiPrestashop />,
+  },
+  {
+    name: 'Git',
+    icon: <SiGit />,
+  },
+  {
+    name: 'Github',
+    icon: <SiGithub />,
+  },
+  {
+    name: 'Photoshop',
+    icon: <SiAdobephotoshop />,
+  },
+  {
+    name: 'Illustrator',
+    icon: <SiAdobeillustrator />,
+  },
+]
+
+export const learn = [
+  {
+    name: 'Angular.Js',
+    icon: <SiAngularjs />,
+  },
+  {
+    name: 'Material UI',
+    icon: <SiMaterialUi />,
+  },
+  {
+    name: 'Firebase',
+    icon: <SiFirebase />,
+  },
+  {
+    name: 'GraphQL',
+    icon: <SiGraphql />,
+  },
+  {
+    name: 'TypeScript',
+    icon: <SiTypescript />,
+  },
+  {
+    name: 'MongoDB',
+    icon: <SiMongodb />,
+  },
+  {
+    name: 'Vue.Js',
+    icon: <SiVueDotJs />,
+  },
+  {
+    name: 'Angular.Js',
+    icon: <SiAngularjs />,
+  },
+  {
+    name: 'Sass',
+    icon: <SiSass />,
+  },
+  {
+    name: 'Socket.Io',
+    icon: <SiSocketDotIo />,
+  },
+]  
+
+export default skills;
