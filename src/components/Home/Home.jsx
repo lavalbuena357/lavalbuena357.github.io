859900b827b891d814b6b2363e1d7e0243893a69
lavalbuena357 <cuervo357@gmail.com>
2022-03-02 15:02:35 -0500

    componente home

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
+import Nav from '../Nav/Nav'
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
