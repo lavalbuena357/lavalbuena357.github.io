e90f41e456bc1c1deba1634b7c35ef9b613fe8d5
lavalbuena357 <cuervo357@gmail.com>
2022-03-02 15:03:02 -0500

    add key on map

--- src/components/Header/SocialBar/SocialBar.jsx
+++ src/components/Header/SocialBar/SocialBar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react'
 import style from './SocialBar.module.css'
 import { social } from '../../../utils/SocialData.js'
@@ -8,7 +9,7 @@ function SocialBar() {
       <div className={style.social_container}>
         {social.map((el, i) => {
           return (
-            <a href={el.link} target='_blank' id={i}>{el.icon}</a>
+            <a key={i} href={el.link} target='_blank' id={i}>{el.icon}</a>
           )
         })}
       </div>
