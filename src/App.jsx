98193fe5b847d0f0536301c767ffab20743fccca
lavalbuena357 <cuervo357@gmail.com>
2022-03-02 15:37:35 -0500

    correccion en rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import './App.css';
 import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home/Home'
+import Projects from './components/Projects/Projects';
+import Main from './components/Main/Main';
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Nav />
       <Routes>
-        <Route path='/' element={<Home /> } />
+        <Route path='/' element={<Main /> } />
+        <Route path='/portafolio' element={<Projects /> } />
       </Routes>
+      <Footer />
     </div>
   )
 }
