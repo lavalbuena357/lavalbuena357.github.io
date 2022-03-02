ce536595b95bcf97e733d84f6c289336d6484e2a
lavalbuena357 <cuervo357@gmail.com>
2022-03-02 15:04:10 -0500

    rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css';
 import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home/Home';
+import Home from './components/Home/Home'
 
 function App() {
   return (
