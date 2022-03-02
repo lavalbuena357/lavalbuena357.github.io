45403f1bf317d25b4bffe537d0c115e66a033082
lavalbuena357 <cuervo357@gmail.com>
2022-03-02 15:03:30 -0500

    tutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './App.css';
 import { Route, Routes } from 'react-router-dom'
-import Header from './components/Header/Header'
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Routes>
-        <Route path='/' element={<Header /> } />
+        <Route path='/' element={<Home /> } />
       </Routes>
     </div>
   )
