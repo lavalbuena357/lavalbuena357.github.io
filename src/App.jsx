fcd9c3ae5a3b53074561d21eaa8c4171b5cd6098
lavalbuena357 <cuervo357@gmail.com>
2022-03-02 13:43:59 -0500

    router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header/Header'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Header /> } />
+      </Routes>
+    </div>
   )
 }
 
