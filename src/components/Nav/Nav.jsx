73123b2cc1152687d7d0985d41cf98afc6663004
lavalbuena357 <cuervo357@gmail.com>
2022-03-02 15:02:47 -0500

    cambios menores

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,48 +1,46 @@
-import React, { useState } from 'react';
-import styles from './Nav.module.css';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import style from './Nav.module.css'
+import { Link } from 'react-router-dom'
 
 function Nav() {
-    const [state, setState] = useState({active: 'about'})
+  const [state, setState] = useState({active: 'about'})
 
-    function handleClick(e) {
-        setState({
-            active: e.target.id
-        })
-    }
+  function handleClick(e) {
+    setState({active: e.target.id})
+  }
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.menu_container}>
-                <ul>
-                    <li>
-                        <Link 
-                            to='/' 
-                            className={state.active === 'about' ? styles.active : styles.menu_button}
-                            id='about'
-                            onClick={e => handleClick(e)}
-                        >Sobre mi</Link>
-                    </li>
-                    <li>
-                        <Link 
-                            to='/portafolio' 
-                            className={state.active === 'projects' ? styles.active : styles.menu_button}
-                            id='projects'
-                            onClick={handleClick}
-                        >Proyectos</Link>
-                    </li>
-                    <li>
-                        <Link 
-                            to='/contacto' 
-                            className={state.active === 'contact' ? styles.active : styles.menu_button}
-                            id='contact'
-                            onClick={handleClick}
-                        >Contacto</Link>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
-};
+  return (
+    <div className={style.container}>
+      <div className={style.menu_container}>
+        <ul>
+          <li>
+            <Link 
+              to='/' 
+              className={state.active === 'about' ? style.active : style.menu_button}
+              id='about'
+              onClick={e => handleClick(e)}
+            >Sobre mi</Link>
+          </li>
+          <li>
+            <Link 
+              to='/portafolio' 
+              className={state.active === 'projects' ? style.active : style.menu_button}
+              id='projects'
+              onClick={handleClick}
+            >Proyectos</Link>
+          </li>
+          <li>
+            <Link 
+              to='/contacto' 
+              className={state.active === 'contact' ? style.active : style.menu_button}
+              id='contact'
+              onClick={handleClick}
+            >Contacto</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
 
-export default Nav;
+export default Nav
