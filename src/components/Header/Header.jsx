b3e0966e4e711ef48b79d6eaa4f0081b89f5cf55
lavalbuena357 <cuervo357@gmail.com>
2022-03-02 13:58:33 -0500

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import styles from './Header.module.css';
+import React from 'react'
+import style from './Header.module.css'
+import SocialBar from './SocialBar/SocialBar'
 import logo from '../../utils/images/logo-av.svg';
 import { Link } from 'react-router-dom';
-import SocialBar from './SocialBar/SocialBar';
 
 function Header() {
   return (
-    <div className={styles.container}>
-      <div className={styles.img_container}>
+    <div className={style.container}>
+      <div className={style.img_container}>
         <Link to='/'>
           <img src={logo} alt="logo albert valbuena" />
-          <div className={styles.logo_text}>
+          <div className={style.logo_text}>
             <span>Albert Valbuena</span>
             <span>Full Stack Developer</span>
           </div>
@@ -19,6 +19,6 @@ function Header() {
       <SocialBar />
     </div>
   )
-};
+}
 
-export default Header;
+export default Header
